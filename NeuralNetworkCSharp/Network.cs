3453f1829f8d3e6e194af8a3797168899e62857e
Muhammad Juniarto <muhammad.juniarto@formulatrix.com>
2024-12-19 14:54:09 +0700

    initial commit and finish feed forward nn scheme

---
+++ NeuralNetworkCSharp/Network.cs
@@ -0,0 +1,98 @@
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace NeuralNetworkCSharp;
+
+public class Network
+{
+    private int _numLayers;
+    private List<int> _sizes;
+    private List<List<double>> _biases { get; set; }
+    private List<List<List<double>>> _weights { get; set; }
+    
+    public Network(List<int> sizes)
+    {
+        _sizes = sizes;
+        _numLayers = sizes.Count;
+        _biases = GenerateInitialBiases(_sizes);
+        _weights = GenerateWeights(_sizes);
+    }
+
+    public List<double> ApplySigmoid(List<double> input)
+    {
+        return input.Select(SigmoidKernelFunction).ToList();
+    }
+
+    public List<double> FeedForward(List<double> input)
+    {
+        List<double> a = new List<double>(input);
+        
+        // Iterate in each layer
+        for (int i = 0; i < _biases.Count ; i++)
+        {
+            List<double> biasCurrentLayer = _biases[i];
+            List<List<double>> weightCurrentLayer = _weights[i];
+            
+            List<double> activationValuesCurrentLayer = new List<double>();
+            
+            // Calculate the value of the activations layer for this current layer stage
+            for (int j = 0; j < weightCurrentLayer.Count; j++)
+            {
+                var weightVector = Vector<double>.Build.Dense(weightCurrentLayer[j].ToArray());
+                var activationVector = Vector<double>.Build.Dense(a.ToArray());
+
+                // Calculate the dot product of the weights and activations
+                double value = weightVector.DotProduct(activationVector) + biasCurrentLayer[j];
+                
+                activationValuesCurrentLayer.Add(SigmoidKernelFunction(value));
+            }
+            
+            a = activationValuesCurrentLayer;
+        }
+
+        return a;
+    }
+    
+    private List<List<double>> GenerateInitialBiases(List<int> sizes)
+    {
+        List<List<double>> biases = new List<List<double>>();
+        for (int i = 1; i < _numLayers; i++)
+        {
+            List<double> layerBiases = new List<double>();
+            var normalDist = new Normal(0, 1);
+
+            for (int j = 0; j < sizes[i]; j++)
+            {
+                layerBiases.Add(normalDist.Sample());
+            }
+            biases.Add(layerBiases);
+        }
+        return biases;
+    }
+    private List<List<List<double>>> GenerateWeights(List<int> sizes)
+    {
+        List<List<List<double>>> weights = new ();
+        for (int i = 1; i < _numLayers; i++)
+        {
+            List<List<double>> layerWeights = new List<List<double>>();
+
+            for (int j = 0; j < sizes[i]; j++)
+            {
+                List<double> neuronWeights = new List<double>();
+                var normalDist = new Normal(0, 1);
+
+                for (int k = 0; k < sizes[i - 1]; k++)
+                {
+                    neuronWeights.Add(normalDist.Sample());
+                }
+                layerWeights.Add(neuronWeights);
+            }
+            weights.Add(layerWeights);
+        }
+        return weights;
+    }
+    private double SigmoidKernelFunction(double x)
+    {
+        return 1 / (1 + Math.Exp(-x));
+    }
+}
---
+++ NeuralNetworkCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using NeuralNetworkCSharp;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int inputLayer = 5;
+        int hiddenLayer = 15;
+        int outputLayer = 4;
+        List<int> networkSize = new List<int>(){ inputLayer, hiddenLayer, outputLayer };
+        
+        Network network = new(networkSize);
+        
+        List<double> input = new List<double>() {1,2,3,5,6} ;
+        List<double> result = network.FeedForward(input);
+        foreach (double value in result)
+        {
+            Console.WriteLine(value);
+        }
+        
+    }
+}
