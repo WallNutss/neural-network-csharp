f2732d6a00c210081acd9afbdc5070c25d4226be
Muhammad Juniarto <muhammad.juniarto@formulatrix.com>
2024-12-28 17:41:31 +0700

    adding train process to main_program

--- NeuralNetworkCSharp/Program.cs
+++ NeuralNetworkCSharp/Program.cs
@@ -1,42 +1,26 @@
 ﻿
 using System.Diagnostics;
 using NeuralNetworkCSharp;
+using NeuralNetworkCSharp.Domain;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        int inputLayer = 784;
-        int hiddenLayer = 15;
-        int outputLayer = 10;
+        const int inputLayer = 784;
+        const int hiddenLayer = 200;
+        const int outputLayer = 10;
         List<int> networkSize = new List<int>(){ inputLayer, hiddenLayer, outputLayer };
         
         Network network = new(networkSize);
         
-        ImageProcessing imageProcessing = new ImageProcessing();
-        var imagesBytes = imageProcessing.SingleImageProcessing("C:/Jun Local Things/Playground/Neural Network C#/mnist_png/train/1/711.png");
-        var imagesArray = imagesBytes.ToList<double>();
-        Console.WriteLine($"Images size: {imagesBytes.Length}");
-        
-        // List<double> input = new List<double>() {0.02, 0.04} ;
-        List<double> result = network.FeedForward(imagesArray);
-        foreach (double value in result)
-        {
-            Console.WriteLine(value);
-        }
-        
         // Load training folder
-        // NOTE : DOES using double[] is a type of Unmanaged Memory?
-        var watch = Stopwatch.StartNew();
         var trainingDirectory = "C:/Jun Local Things/Playground/Neural Network C#/mnist_png/train";
-        (List<string> imagePaths, List<int[]> labelEncoding) = network.LoadDataset(trainingDirectory);
-        List<double[]> trainingImages = imageProcessing.BatchImageProcessing(imagePaths);
-        watch.Stop();
         
-        var elapsedS = watch.ElapsedMilliseconds/1000f;
-        Console.WriteLine("Finished loading training data....");
-        Console.WriteLine($"Dataset count : {imagePaths.Count}");
-        Console.WriteLine($"Loading training data took {elapsedS} seconds.");
+        // Start training process
+        network.Train(trainingDirectory, 100, 1000);
         
+        // Test the network model
+
     }
 }
